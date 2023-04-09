911f737a4500283459599df72e1d054c95e7a804
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-04-09 12:12:18 +0700

    Create API

--- main.js
+++ main.js
@@ -1,37 +1,112 @@
+/*
+
+    ==================================
+    |      ╔═╗╦╔╗ ╔═╗╦ ╦╔╦╗╦╔═╗      |
+    |      ╚═╗║╠╩╗╚═╗║ ║ ║ ║╚═╗      |
+    |      ╚═╝╩╚═╝╚═╝╚═╝ ╩ ╩╚═╝      |
+    |    ╔═╗╔═╗╦ ╦╔═╗╔╦╗╦ ╦╦  ╔═╗    |
+    |    ╚═╗║  ╠═╣║╣  ║║║ ║║  ║╣     |
+    |    ╚═╝╚═╝╩ ╩╚═╝═╩╝╚═╝╩═╝╚═╝    |
+    |            ╔═╗╔═╗╦             |
+    |            ╠═╣╠═╝║             |
+    |            ╩ ╩╩  ╩             |
+    ==================================
+    Authors:
+    @ Egoshin Alexey        (https://github.com/RipeCherries)
+    @ Nikolaev Vladislav    (https://github.com/Vl1ts)
+
+*/
+
+/* --- Importing libraries --- */
 const express = require("express");
-const fs = require("fs");
+const basicAuth = require('express-basic-auth');
 const bodyParser = require('body-parser');
-const { request, response } = require("express");
+const fs = require("fs");
 
+
+/* --- Importing custom functions --- */
 const getAllGroups = require("./src/utils/getAllGroups");
+const getSchedule = require("./src/utils/getSchedule");
 
+
+/* --- Creating an API application --- */
 const app = express();
-app.use(express.static(__dirname + "/public"));
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+// limitation on the size of transmitted data
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000
+}));
 
-const jsonParser = express.json();
 
-/* --- API ENDPOINT`S --- */
-app.get("/api/allGroups", function(request, response) {
-    const data = fs.readFileSync("./src/data/allGroups.json","utf8");
+/* --- Authorization --- */
+const auth = basicAuth({
+    users: { 'admin': 'admin' },
+    challenge: true
+})
+
+
+/* --- API endpoint`s --- */
+// common (main) URL:
+app.get("/", (req, res) => {
+    res.send("API for the SibSUTIS University schedule mobile application. " +
+        "Go to the URL /admin to enter the admin panel.");
+})
+
+// admin URL:
+app.get("/admin", auth, (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+// schedule update URL:
+app.post("/api/scheduleUpload", auth, (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+
+
+    const groupsData = getAllGroups(req.body);
+    fs.writeFileSync("./src/data/allGroups.json", JSON.stringify(groupsData));
+
+    const scheduleData = getSchedule(req.body);
+    fs.writeFileSync("./src/data/schedule.json", JSON.stringify(scheduleData));
+
+
+    const d = new Date();
+    const tmp = {
+        date: d.getTime()
+    }
+
+    fs.writeFileSync("./src/data/date.json", JSON.stringify(tmp));
+
+    return res.sendStatus(200);
+});
+
+// get list of group name`s URL:
+app.get("/api/allGroups", (req, res) => {
+    const data = fs.readFileSync("./src/data/allGroups.json", "utf8");
     const groups = JSON.parse(data);
-    response.send(groups);
+    res.send(groups);
 });
 
-app.post("/api/scheduleUpload", jsonParser, (request, response) => {
-   if (!request.body) {
-       return response.sendStatus(400);
-   }
-
-   const data = getAllGroups(request.body);
-
-   fs.writeFileSync("./src/data/allGroups.json", JSON.stringify(data));
-
-   return response.sendStatus(200);
+// get schedule URL:
+app.get("/api/schedule", (req, res) => {
+    const data = fs.readFileSync("./src/data/schedule.json", "utf8");
+    const schedule = JSON.parse(data);
+    res.send(schedule);
 });
 
+// get last update date URL:
+app.get("/api/lastUpdate", (req, res) => {
+    const data = fs.readFileSync("./src/data/date.json", "utf8");
+    const date = JSON.parse(data);
+    res.send(date);
+})
+
+
+/* --- Start app --- */
 app.listen(3000, () => {
     console.log("The server is waiting for connection");
 })
