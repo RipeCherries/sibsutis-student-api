5f6ede76e5ddb62087fb51cf68648a7a6d6f1fd0
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-07-27 15:20:33 +0700

    Added a schema for the group collection

---
+++ models/group.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  groups: {
+    type: [String],
+    required: [true, 'Поле groups должно быть заполнено!']
+  }
+});
+
+const Group = mongoose.model("Group", groupSchema, "groups");
+
+module.exports = Group;
