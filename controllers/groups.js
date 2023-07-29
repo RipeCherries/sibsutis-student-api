0faaa107c239d2893e7ee65a871a343ab93d1f77
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-07-29 16:10:43 +0700

    Added a group controller to work with the database

---
+++ controllers/groups.js
@@ -0,0 +1,37 @@
+const Group = require('../models/group');
+
+module.exports.getGroups = (req, res) => {
+  Group.find()
+    .then((groups) => {
+      if (groups.length === 0) {
+        return res.status(404).json({ message: 'Данные о группах отсутствуют' });
+      }
+
+      res.send(groups);
+    })
+    .catch((error) => res.status(500).json({
+      message: 'Произошла ошибка при получении данных из БД',
+      error: error.message,
+    }));
+};
+
+module.exports.updateGroups = (req, res) => {
+  const newData = req.body;
+
+  if (!newData || Object.keys(newData).length === 0) {
+    return res.status(400).json({ message: 'Не предоставлены данные для обновления' });
+  }
+
+  Group.findOneAndUpdate({}, newData, { new: true })
+    .then((updatedData) => {
+      if (updatedData.length === 0) {
+        return res.status(400).json({ message: 'Документ в коллекции не найден' });
+      }
+
+      res.json({ status: 'success', data: updatedData });
+    })
+    .catch((error) => res.status(500).json({
+      message: 'Произошла ошибка при обновлении данных в БД',
+      error: error.message,
+    }));
+};
