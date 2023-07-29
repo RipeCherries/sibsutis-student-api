f93530df69b2209b5868274200d76e33009cfe30
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-07-29 16:08:56 +0700

    Fixed the codestyle

--- models/group.js
+++ models/group.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const groupSchema = new mongoose.Schema({
   groups: {
     type: [String],
-    required: [true, 'Поле groups должно быть заполнено!']
-  }
+    required: [true, 'Поле groups должно быть заполнено!'],
+  },
 });
 
-const Group = mongoose.model("Group", groupSchema, "groups");
+const Group = mongoose.model('Group', groupSchema, 'groups');
 
 module.exports = Group;
