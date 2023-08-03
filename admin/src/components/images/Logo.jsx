26d45eb2f557b36905f5b16cf13b7732d8b9bbe3
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-03 13:53:56 +0700

    Create app logo

---
+++ admin/src/components/images/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+    return (
+        <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="39" cy="30" r="28.5" stroke="white" strokeWidth="3"/>
+            <circle cx="50" cy="41" r="28.5" stroke="white" strokeWidth="3"/>
+            <circle cx="41" cy="50" r="28.5" stroke="white" strokeWidth="3"/>
+            <circle cx="30" cy="41" r="28.5" stroke="white" strokeWidth="3"/>
+        </svg>
+    );
+}
+
+export default Logo;
