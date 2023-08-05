03a7996672505189fee4bc731e608c47d82db90b
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-05 15:15:04 +0700

    Add render update section

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,6 +3,7 @@ import '@fontsource-variable/montserrat';
 import './App.css';
 import Header from "./components/header/Header";
 import LastUpdateSection from "./components/lastUpdateSection/LastUpdateSection";
+import UpdateSection from "./components/updateSection/UpdateSection";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <div className="container">
             <Header />
             <LastUpdateSection />
+            <UpdateSection />
         </div>
     </div>
   );
