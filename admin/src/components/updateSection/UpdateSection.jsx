43f9b54483984d3ad388deb3d447edc6558923fd
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-09 18:47:55 +0700

    Rewrite update section with redux

--- admin/src/components/updateSection/UpdateSection.jsx
+++ admin/src/components/updateSection/UpdateSection.jsx
@@ -1,21 +1,28 @@
-import React, {useState} from "react";
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
 import "./update-section.css";
 import DropFileInput from "../drop-file-input/DropFileInput";
 import CalendarPicker from "../calendar-picker/CalendarPicker";
+import {setStartOfSemester} from "../../store/startOfSemesterReducer";
 
 const UpdateSection = () => {
-    const [date, setDate] = useState(new Date());
+    const startDate = useSelector(state => state.startOfSemester.date);
+    const dispatch = useDispatch();
+
+    const handleDateChange = (newDate) => {
+        dispatch(setStartOfSemester(newDate));
+    }
 
     return (
         <section className="update-section">
             <h2 className="update-section__title">Обновление данных расписания:</h2>
             <div className="update-section__controls">
                 <div className="update-section__file-upload">
-                    <DropFileInput />
+                    <DropFileInput/>
                 </div>
                 <div className="update-section__date-picker">
                     <h5 className="update-section__date-picker__title">Дата начала семестра:</h5>
-                    <CalendarPicker value={date} onChange={setDate} />
+                    <CalendarPicker value={startDate} onChange={handleDateChange}/>
                 </div>
             </div>
         </section>
