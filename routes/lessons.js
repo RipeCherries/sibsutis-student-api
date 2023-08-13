b35ad858383680a84f339ad62643cae6b0646a02
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-13 17:44:25 +0700

    Add update lessons route

--- routes/lessons.js
+++ routes/lessons.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getLessons, getLessonsByGroupId } = require('../controllers/lessons');
+const { getLessons, getLessonsByGroupId, updateLessons} = require('../controllers/lessons');
 
 const lessonsRouter = express.Router();
 
 lessonsRouter.get('/', getLessons);
+lessonsRouter.put('/', updateLessons);
 lessonsRouter.get('/:groupId', getLessonsByGroupId);
 
 module.exports = lessonsRouter;
