1439aaaf159d04c56098b5e516a0ad181e563c4f
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-13 17:43:58 +0700

    Install cors

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { PORT, DB_PATH } = require('./utils/config');
+const routes = require('./routes/index');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json({limit: "10mb"}));
+app.use(bodyParser.urlencoded({limit: "10mb", extended: true}));
+
+app.use(routes);
 
 mongoose
   .connect(DB_PATH, {
@@ -15,5 +23,11 @@ mongoose
   })
   .then(() => console.log('Подключение к базе данных: Успешно!'))
   .catch((err) => console.log('Подключение к базе данных: Неудачно!', err));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 app.listen(PORT, () => console.log(`API запущено на порту ${PORT}`));
