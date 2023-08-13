dbfacb15ab5f91eb45961e8bc55b70fa91b542df
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-13 17:45:14 +0700

    Create upldate lessons func

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -15,6 +15,33 @@ module.exports.getLessons = (req, res) => {
     }));
 };
 
+module.exports.updateLessons = (req, res) => {
+  const newData = req.body;
+
+  if (!newData || Object.keys(newData).length === 0) {
+    return res.status(400).json({ message: 'Не предоставлены данные для обновления' });
+  }
+
+  Lesson.deleteMany({})
+    .catch((error) => res.status(500).json({
+      message: 'Произошла ошибка при обновлении данных в БД',
+      error: error.message,
+    }));
+
+  Lesson.insertMany(newData)
+    .then((updatedData) => {
+      if (updatedData.length === 0) {
+        return res.status(400).json({ message: 'Документы в коллекции не найдены' });
+      }
+
+      res.json({ status: 'success', data: updatedData });
+    })
+    .catch((error) => res.status(500).json({
+      message: 'Произошла ошибка при обновлении данных в БД',
+      error: error.message,
+    }));
+};
+
 module.exports.getLessonsByGroupId = (req, res) => {
   const targetGroupId = req.params.groupId;
 
