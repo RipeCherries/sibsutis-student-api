64feb8a11b301534d9e7c2e0159285d244eef4ce
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-14 17:19:50 +0700

    Finish last update section

--- admin/src/components/lastUpdateSection/LastUpdateSection.jsx
+++ admin/src/components/lastUpdateSection/LastUpdateSection.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import { PiCalendarFill } from "react-icons/pi";
 
 import "./last-update-section.css";
+import axios from "axios";
+import {setLastUpdate} from "../../store/lastUpdateReducer";
 
 const LastUpdateSection = () => {
+    const lastUpdate = useSelector(store => store.lastUpdate.date);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/last-update').then(response => {
+            const data = response.data;
+            const lastUpdateDate = new Date(data.date);
+            dispatch(setLastUpdate(lastUpdateDate));
+        }).catch(error => {
+           console.log(error);
+        });
+    }, [dispatch]);
+
     return (
       <section className="last-update-section">
           <div className="last-update-section__info">
               <PiCalendarFill size={40} />
               <p className="last-update-section__info__text">Дата последнего обновления:</p>
-              <p className="last-update-section__info__date">04.08.2023 11:25</p>
+              {
+                  lastUpdate ? (
+                      <p className="last-update-section__info__date">{lastUpdate.toLocaleString()}</p>
+                  ) : (
+                      <p className="last-update-section__info__date">Загрузка...</p>
+                  )
+              }
           </div>
-
       </section>
     );
 }
