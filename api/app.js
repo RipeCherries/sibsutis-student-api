a41d6e775740520705d5eaea0c4d835be57859b1
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-08-17 12:24:37 +0700

    Test

--- api/app.js
+++ api/app.js
@@ -15,6 +15,9 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(routes);
+app.get('/', (req, res) => {
+    res.send("test");
+})
 app.use('/admin', express.static(path.join(__dirname, 'admin-build')));
 app.use('/static', express.static(path.join(__dirname, 'admin-build', 'static')));
 
