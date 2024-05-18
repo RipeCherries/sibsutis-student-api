d4d3abc7e740483d085dbd813eefe32800cb2c74
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-05-18 19:55:27 +0700

    Added an import and export file

---
+++ account/src/open-api/index.ts
@@ -0,0 +1,25 @@
+import getAllGroups from './group/getAllGroups';
+import getGroupByID from './group/getGroupByID';
+import updateGroups from './group/updateGroups';
+import createUser from './user/createUser';
+import deleteUser from './user/deleteUser';
+import getAllUsers from './user/getAllUsers';
+import getUserByID from './user/getUserByID';
+import loginUser from './user/loginUser';
+import refreshTokens from './user/refreshTokens';
+import updateUser from './user/updateUser';
+import updateUserPassword from './user/updateUserPassword';
+
+export {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUserByID,
+  loginUser,
+  updateUser,
+  updateUserPassword,
+  refreshTokens,
+  getAllGroups,
+  getGroupByID,
+  updateGroups,
+};
