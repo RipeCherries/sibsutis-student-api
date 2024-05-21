c3acbf60b005f74de51f85e4da2261da6aee2338
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-05-21 12:12:16 +0700

    Added a router for lessons

---
+++ schedule/src/routes/lessonRouter.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import LessonController from '../controllers/lessonController';
+
+const router: express.Router = express.Router();
+const controller: LessonController = new LessonController();
+
+// Обновление данных о занятиях
+router.put('/lessons', controller.updateLessons.bind(controller));
+
+// Получение расписания занятий по названию группы
+router.get('/lessons/:groupName', controller.getLessonsByGroupName.bind(controller));
+
+// Получение всего расписания занятий
+router.get('/lessons', controller.getAllLessons.bind(controller));
+
+export default router;
