066a84976f0d53f6474265892bc16c54c04a4ea1
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2024-05-22 15:36:04 +0700

    Added the lesson data type

---
+++ update-data/src/types/lesson.ts
@@ -0,0 +1,10 @@
+export interface Lesson {
+  name: string;
+  type: string;
+  teacher: string;
+  classroom: string;
+  group: string;
+  week: string;
+  weekday: string;
+  startHour: string;
+}
